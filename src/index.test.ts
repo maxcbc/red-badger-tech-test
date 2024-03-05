60ff0aa707ebef17142b438520c98783f64f7eb2
Max Bladen-Clark <maxcbc@live.com>
2024-03-05 21:29:35 +0000

    add entry point loading input and writing output

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,26 @@
-describe('example test', () => {
-    test('unity', () => {
+import fs from 'node:fs/promises'
+import {main} from "./index";
+
+jest.mock('node:fs/promises')
+
+const mocks = {
+    readFile: jest.mocked(fs.readFile),
+    writeFile: jest.mocked(fs.writeFile),
+}
+
+describe('main()', () => {
+    test('it produces the correct output for the test input', async() => {
         // Arrange
-        const value = 1;
+        mocks.readFile.mockResolvedValue("5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL".trim())
 
         // Act
-        const actual = value === 1
+        await main()
 
         // Assert
-        expect(actual).toBeTruthy()
+        expect(mocks.writeFile).toHaveBeenCalledWith(
+            expect.stringMatching(/\/output.txt$/),
+            "1,1,E\n3,3,N\n2,4,S",
+            'utf-8',
+        )
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,52 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import {Grid} from "./grid/grid";
+import {Robot} from "./robot/robot";
 
-export function helloWorld(): string {
-    return 'hello world'
+export async function main() {
+    const rawInput= await fs.readFile(path.join(process.cwd(), 'input.txt' ), "utf-8")
+    const gridConfig = rawInput.slice(0, rawInput.indexOf('\n')).trim().split('').map(s => parseInt(s))
+    const robotConfigs = rawInput.slice(rawInput.indexOf('\n') + 1).trim().split('\n')
+
+
+    const grid = new Grid({
+        size: {
+            x: gridConfig[0],
+            y: gridConfig[1]
+        }
+    })
+
+    const robotPositions: string[] = []
+    let i = 1
+    while (i <= robotConfigs.length) {
+        const config = robotConfigs[i - 1].trim().split(' ').map(s => s.trim())
+        const instructions =  robotConfigs[i].split('')
+        const robot = new Robot({
+                x: parseInt(config[0]),
+                y: parseInt(config[1]),
+                cardinalPoint: config[2]
+        })
+
+        for (const instruction of instructions) {
+            if (instruction === 'F') {
+                robot.moveForward()
+            } else if (instruction === 'R') {
+                robot.turnRight()
+            } else if (instruction === 'L') {
+                robot.turnLeft()
+            }
+        }
+
+        robotPositions.push(robot.position)
+        i += 2
+    }
+
+    fs.writeFile(path.join(process.cwd(), 'output.txt' ), robotPositions.join('\n'), 'utf-8')
+
+
+}
+
+if (require.main === module) {
+   main()
+       .catch(console.error)
 }
